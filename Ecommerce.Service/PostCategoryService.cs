3f60b3ceb734b653b8047a96e506bfe311270d68
nghiepuit <nghiepuit@gmail.com>
2017-02-27 15:50:06 +0700

    Khởi Tạo Project

---
+++ Ecommerce.Service/PostCategoryService.cs
@@ -0,0 +1,78 @@
+﻿using Ecommerce.Data.Infrastructure;
+using Ecommerce.Data.Repositories;
+using Ecommerce.Model.Models;
+using System.Collections.Generic;
+
+namespace Ecommerce.Service
+{
+    public interface IPostCategoryService
+    {
+        void add(PostCategory postCategory);
+
+        void Update(PostCategory postCategory);
+
+        void Delete(int id);
+
+        IEnumerable<PostCategory> GetAll();
+
+        IEnumerable<PostCategory> GetAllByParentId(int parentId);
+
+        IEnumerable<PostCategory> GetAllPaging(int page, int pageSize, out int totalRow);
+
+        PostCategory GetById(int id);
+
+        void SaveChangees();
+    }
+
+    public class PostCategoryService : IPostCategoryService
+    {
+        private IPostCategoryRepository _postCategoryRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public PostCategoryService(IPostCategoryRepository postCategoryRepository, IUnitOfWork unitOfWork)
+        {
+            this._postCategoryRepository = postCategoryRepository;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public void add(PostCategory postCategory)
+        {
+            _postCategoryRepository.Add(postCategory);
+        }
+
+        public void Delete(int id)
+        {
+            _postCategoryRepository.Delete(id);
+        }
+
+        public IEnumerable<PostCategory> GetAll()
+        {
+            return _postCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<PostCategory> GetAllByParentId(int parentId)
+        {
+            return _postCategoryRepository.GetMulti(x => x.Status && x.ParentID == parentId);
+        }
+
+        public IEnumerable<PostCategory> GetAllPaging(int page, int pageSize, out int totalRow)
+        {
+            return _postCategoryRepository.GetMultiPaging(x => x.Status, out totalRow, page, pageSize);
+        }
+
+        public PostCategory GetById(int id)
+        {
+            return _postCategoryRepository.GetSingleById(id);
+        }
+
+        public void SaveChangees()
+        {
+            _unitOfWork.Commit();
+        }
+
+        public void Update(PostCategory postCategory)
+        {
+            _postCategoryRepository.Update(postCategory);
+        }
+    }
+}
